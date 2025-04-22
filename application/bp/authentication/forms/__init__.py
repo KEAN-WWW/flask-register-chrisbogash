298f64a2f56366bcd2c92d200030027d38ae5631
chrisbogash <126812173+chrisbogash@users.noreply.github.com>
2025-04-22 13:37:55 -0400

    first commit

--- application/bp/authentication/forms/__init__.py
+++ application/bp/authentication/forms/__init__.py
@@ -1,6 +1,27 @@
 from flask_wtf import FlaskForm
+from wtforms import validators
+from wtforms.fields import *
 
 
 class RegisterForm(FlaskForm):
+    email = EmailField(label="Email Address",
+                       validators=[validators.DataRequired()],
+                       description="You need to signup with an email"
+                       )
+
+    password = PasswordField(label="Create Your Password",
+                             validators=[
+                                 validators.DataRequired(),
+                             ])
+
+    confirm = PasswordField(label="Confirm Your Password",
+                            validators=[
+                                validators.DataRequired(),
+                                validators.EqualTo("password")
+                            ]
+                            )
+
+    submit = SubmitField()
+
     pass
 
